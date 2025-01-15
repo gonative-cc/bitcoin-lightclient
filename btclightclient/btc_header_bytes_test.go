33cb7cb9e6232582b5fa83b5a3c7ca9d4b7de623
Vu Vo <vu.voth@gmail.com>
2025-01-15 17:57:28 +0700

    more tests
    Signed-off-by: Vu Vo <vu.voth@gmail.com>

--- btclightclient/btc_header_bytes_test.go
+++ btclightclient/btc_header_bytes_test.go
@@ -1,6 +1,7 @@
 package btclightclient
 
 import (
+	"encoding/hex"
 	"testing"
 
 	"gotest.tools/assert"
@@ -37,6 +38,12 @@ func TestBTCHeaderFromHex(t *testing.T) {
 			headerHex:         "020000004cdba1415b2c6e7808c1b3c18df1374238454f7104203475bf01000000000000c17ea9d06015dc83902911cd24837a8ba4bdc0c1d72b873f906d921e06e48d2f984a8250ef75051a72a8061ab",
 			expectedError:     ErrInvalidHeaderSize,
 			expectedBlockHash: "0000000000000363d7f5f3341fb0b5b69949103e2d681591c9f737e4ea67e2a7",
+		},
+		{
+			name:              "non hex data",
+			headerHex:         "020000004cdba1415b2c6e7808c1b3c18df1374238454f7104203475bf01000000000000c17ea9d06015dc83902911cd24837a8ba4bdc0c1d72b873f906d921e06e48d2f984a8250ef75051a72a8061Q",
+			expectedError:     hex.InvalidByteError('Q'),
+			expectedBlockHash: "",
 		},
 	}
 
