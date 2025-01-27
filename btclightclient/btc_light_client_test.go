5a3fdba20dec1d0d9774d19554527ce51f9fc5ba
udit-gulati <uditgulati6@gmail.com>
2025-01-27 18:36:04 +0530

    add comments to test cases explaining their behaviour
    Signed-off-by: udit-gulati <uditgulati6@gmail.com>

--- btclightclient/btc_light_client_test.go
+++ btclightclient/btc_light_client_test.go
@@ -109,15 +109,19 @@ func TestLatestFinalizedBlock(t *testing.T) {
 		BlockHash string
 	}
 	testCases := map[string]TestCase{
-		"Append a fork":                      {nil, 11, "6393fcb4ba7189f914c2f74fad2bd0ef7743c867b4a81db458f9f9e506458fe7"},
-		"Create fork":                        {nil, 10, "7a47c3a083add37394061eba8dbfb1fe2026d3fed6bd3d428b043b515bcb269e"},
+		// adds new block to longest chain, increasing finalised block height by 1
+		"Append a fork": {nil, 11, "6393fcb4ba7189f914c2f74fad2bd0ef7743c867b4a81db458f9f9e506458fe7"},
+		// adds new block to fork chain, keeping finalised block height same
+		"Create fork": {nil, 10, "7a47c3a083add37394061eba8dbfb1fe2026d3fed6bd3d428b043b515bcb269e"},
+		// fails new block addition, keeping finalised block height same
 		"Insert failed because fork too old": {ErrForkTooOld, 10, "7a47c3a083add37394061eba8dbfb1fe2026d3fed6bd3d428b043b515bcb269e"},
-		"Block doesn't belong to any fork!":  {ErrParentBlockNotInChain, 10, "7a47c3a083add37394061eba8dbfb1fe2026d3fed6bd3d428b043b515bcb269e"},
+		// no new block in forks, keeping finalised block height same
+		"Block doesn't belong to any fork!": {ErrParentBlockNotInChain, 10, "7a47c3a083add37394061eba8dbfb1fe2026d3fed6bd3d428b043b515bcb269e"},
 	}
 
 	run := func(t *testing.T, name string, tc TestCase) {
 		data := commonTestCase[name]
-
+		g
 		lc := initLightClient(data.headers)
 		btcHeader, _ := BlockHeaderFromHex(data.header)
 		lcErr := lc.InsertHeader(btcHeader)
